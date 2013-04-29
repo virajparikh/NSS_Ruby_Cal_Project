60df4d96a64461bb495a384895b64fa107904bdf
Viraj Parikh <virajparikh@gmail.com>
2013-04-29 14:05:00 -0500

    testing successfully thru month, year and days header

--- cal.rb
+++ cal.rb
@@ -8,9 +8,12 @@ class Calendar
   attr_reader :month
   attr_reader :year
 
-  def initialize(year)
-  	# @month = month
+  def initialize(month, year)
+  	@month = month
   	@year = year
+    month_hash = { 1 => "January", 2 => "February", 3 => "March", 4 => "April", 5 => "May", 6 => "June", 7 => "July", 8 => "August", 9 => "September", 10 => "October", 11 => "November", 12 => "December" }
+    @month_name = month_hash.fetch(month)
+
   end
 
   def leap_year?
@@ -33,20 +36,20 @@ class Calendar
 	  end
   end
 
-  def number_of_weeks_in_month
-    # if month has 31 days and month begins on Friday or Saturday, then month has 6 weeks
-    # elsif month has 30 days and month begins on Saturday, then month has 6 weeks
-    # elsif month is February, is not a leap year, and begins on Sunday, then month has 4 weeks
-    # else month has 5 weeks
-
+  def month_year_header
+    "#{@month_name} #{year}".center(20)
   end
 
+  def days_header
+    "Su Mo Tu We Th Fr Sa"
+  end
+
+  # def number_of_weeks_in_month
+  #   # if month has 31 days and month begins on Friday or Saturday, then month has 6 weeks
+  #   # elsif month has 30 days and month begins on Saturday, then month has 6 weeks
+  #   # elsif month is February, is not a leap year, and begins on Sunday, then month has 4 weeks
+  #   # else month has 5 weeks
+
+  # end
+
 end
-
-# class Day
-
-# # # days = [ :Su, :Mo, :Tu, :We, :Th, :Fr, :Sa ]
-
-# # class Month
-
-#  months = { 1 => :January,  2 => :February, 3 => :March, 4 => :April, 5 => :May, 6 => :June, 7 => :July, 8 => :August, 9 => :September, 10 => :October, 11 => :November, 12 => :December }  # these should be associated with a numbers.  Use hashes??
--- test/test_cal_integration.rb
+++ test/test_cal_integration.rb
@@ -8,31 +8,35 @@ class CalendarIntegrationTest < Test::Unit::TestCase
   end
 
   def test_02a_leap_year?
-    cal = Calendar.new(2000)
+    cal = Calendar.new(4, 2000)
     assert_equal(true, cal.leap_year?)
   end
 
   def test_02b_leap_year?
-    cal = Calendar.new(1997)
+    cal = Calendar.new(2, 1997)
     assert_equal(false, cal.leap_year?)
   end
 
   def test_02c_leap_year?
-    cal = Calendar.new(2100)
+    cal = Calendar.new(1, 2100)
     assert_equal(false, cal.leap_year?)
   end
 
   def test_02d_leap_year?
-    cal = Calendar.new(2400)
+    cal = Calendar.new(10, 2400)
     assert_equal(true, cal.leap_year?)
   end
-
  
+  def test_3_print_month_year_header
+  	cal = Calendar.new(3, 2013)
+  	assert_equal("     March 2013     ", cal.month_year_header)
+  end
 
-  # def test_3_number_of_weeks_in_month
-  # 	cal = Calendar.new
-  # 	assert_equal(6, `cal 3 2013`)
-  # end
+  def test_4_print_days_header
+  	cal = Calendar.new(3, 2013)
+  	assert_equal("Su Mo Tu We Th Fr Sa", cal.days_header)
+  end
 
+  
 
 end
